e4c3bef08f1354f3b3508cc7af8ca3f1a68bb93b
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-02-23 12:29:22 +0200

    increment async saga, root saga

--- sagas.js
+++ sagas.js
@@ -1,3 +1,20 @@
-export function* helloWorld() {
+import { all, put, takeEvery } from 'redux-saga/effects';
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+function* helloWorld() {
   console.log('Hello world of sagas!');
+}
+
+function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: 'INCREMENT' });
+}
+
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+export default function* rootSage() {
+  yield all([helloWorld(), watchIncrementAsync()]);
 }
